fa5d5d86231743b494878729af9bd2587c4ca7ef
yousefis <sahar@autofilltech.com>
2021-11-18 20:55:54 +0100

    Finalize cuda summation of two vectors

--- add_vectors/src/kernels.cu
+++ add_vectors/src/kernels.cu
@@ -1,2 +1,19 @@
+/*
+ * Developer: Sahar Yousefi
+ * more info: https://github.com/yousefis/cuda_pool
+ */
+
 #include"../include/kernels.cuh"
+#include<iostream>
+__global__
+void sum_vectors(float* a, float* b, float* c, size_t n)
+{
+    int idx = threadIdx.x + blockDim.x * blockIdx.x; //index of the thread on the block and grid
+    if (idx < n)
+    {
+        c[idx] = a[idx] + b[idx];
+        //printf("%d, %f, %d\n",idx,c[idx],n);
+    }
+    
+}
 
